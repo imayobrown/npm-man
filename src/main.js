25ae22384749da0bb48b697d10ddd63b587b788f
ibrown <ibrown@lpmc-ibrown.corp.epnet.com>
2016-07-06 17:51:37 -0400

    Added travis yaml file and small changes to main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
+//Require writable to create write stream
+
+var Writable = require('stream').Writable;
+
+var ws = Writable({'decodeStrings': false});
+
+ws._write = function(chunk, enc, next) {
+    console.log(chunk.toString());
+    next();
+}
 //Requires necessary for file to run
 var fs = require('fs');
 
 //Create read stream from opening the README file
-var readStream = fs.createReadStream('README.md');
+var rs = fs.createReadStream('../README.md');
+
+rs.pipe(ws);
 
 //Pipe the output of the file to the command line
-readStream.pipe(process.stdout);
+//readStream.pipe(process.stdout);
